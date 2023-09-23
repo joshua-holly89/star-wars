a32caf4a2e926409f8ad3197e161c9dd39f155cb
Joshua Holly <joshua.holly89@gmail.com>
2023-09-24 01:36:23 +0200

    fix tests

--- src/app/components/add-person/add-person.component.spec.ts
+++ src/app/components/add-person/add-person.component.spec.ts
@@ -1,6 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AddPersonComponent } from './add-person.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('AddPersonComponent', () => {
   let component: AddPersonComponent;
@@ -8,7 +18,23 @@ describe('AddPersonComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [AddPersonComponent]
+      declarations: [AddPersonComponent],
+      imports: [
+        HttpClientModule,
+        MatListModule,
+        MatCardModule,
+        MatProgressSpinnerModule,
+        MatIconModule,
+        FormsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatDialogModule,
+        BrowserAnimationsModule,
+        ReactiveFormsModule
+      ],
+      providers:[
+        {provide : MatDialogRef, useValue : {}}
+      ]
     });
     fixture = TestBed.createComponent(AddPersonComponent);
     component = fixture.componentInstance;
